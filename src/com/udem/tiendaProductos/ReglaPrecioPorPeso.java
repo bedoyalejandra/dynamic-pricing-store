94347934a1b9ca6b392def326a5c0eff9f9d808e
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2025-03-29 15:11:53 -0500

    Refactor ReglaPrecioPorPeso class: update SKU prefix and calculation logic

--- src/com/udem/tiendaProductos/ReglaPrecioPorPeso.java
+++ src/com/udem/tiendaProductos/ReglaPrecioPorPeso.java
@@ -1,16 +1,17 @@
 package com.udem.tiendaProductos;
 
 public class ReglaPrecioPorPeso implements ReglaPrecio {
-
-    private static final String SKU = "WE";
+    private static final String SKU_PREFIX = "WE";
+    private static final int GRAMOS_POR_KILO = 1000;
 
     @Override
     public Boolean esAplicable(String sku) {
-        return sku.startsWith(SKU);
+        return sku.startsWith(SKU_PREFIX);
     }
 
     @Override
-    public float calcularTotal(int cantidad, float precioUnitario) {
-        return cantidad * precioUnitario; // cantidad en gramos
+    public float calcularTotal(int cantidadEnKilos, float precioPorGramo) {
+        int cantidadEnGramos = cantidadEnKilos * GRAMOS_POR_KILO;
+        return cantidadEnGramos * precioPorGramo;
     }
 }

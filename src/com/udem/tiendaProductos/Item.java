83bf54c7d4b1243d889747ed845dee19461b4ae9
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2025-03-29 14:48:18 -0500

    Implemented item class

--- src/com/udem/tiendaProductos/Item.java
+++ src/com/udem/tiendaProductos/Item.java
@@ -1,14 +1,20 @@
 package com.udem.tiendaProductos;
 
 public class Item {
+    private Producto producto;
     private int cantidad;
-    
-    public Item(Producto producto, int cantidad) {
+    private ReglaPrecio reglaPrecio;
+
+    public Item(Producto producto, int cantidad, ReglaPrecio reglaPrecio) {
+        this.producto = producto;
         this.cantidad = cantidad;
+        this.reglaPrecio = reglaPrecio;
     }
 
-    public float calculartotal() {
-        return cantidad;
-    }    
+    public float calcularTotal() {
+        return reglaPrecio.calcularTotal(cantidad, producto.getPrecioUnitario());
+    }
 
+    public Producto getProducto() { return producto; }
 }
+

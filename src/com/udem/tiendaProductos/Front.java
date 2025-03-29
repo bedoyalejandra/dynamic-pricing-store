c226f24daee53e773d0d5451a1ed75c6eab2c75c
sgraciano <sgraciano@sistecredito.com>
2025-03-29 14:24:28 -0500

    se cambia  el main

--- src/com/udem/tiendaProductos/Front.java
+++ src/com/udem/tiendaProductos/Front.java
@@ -2,7 +2,7 @@ package com.udem.tiendaProductos;
 
 public class Front {
     public static void main(String[] args) {
-        System.out.println("Bienvenido a la tienda de productos!");
+        System.out.println("HELLOOOOO SAMUEL!");
     }
 
 }
